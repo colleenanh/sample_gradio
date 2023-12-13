9f6021bbe91e82c55f040f46d2fec445b55df1c2
colleenanh <143149970+colleenanh@users.noreply.github.com>
2023-12-13 15:57:38 +0800

    Update sample_gradio.py

--- sample_gradio.py
+++ sample_gradio.py
@@ -9,4 +9,4 @@ with gr.Blocks() as demo:
   greet_btn = gr.Button("Greet")
   greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
-demo.launch(server_name = "34.70.191.92", server_port=3389)
+demo.launch(server_name = "0.0.0.0", server_port=3389)
